e89e61c810db6d3910f927f242a898f4d4ef3385
Daniel <Dstout55@gmail.com>
2014-03-16 17:24:10 -0300

    Changed some comments around

--- Filer/src/GUI.java
+++ Filer/src/GUI.java
@@ -33,11 +33,10 @@ import javax.swing.KeyStroke;
 import javax.swing.UIManager;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+//this class implements the action listener and key listener classes
 public class GUI implements ActionListener, KeyListener
 {
-	//this class implements the action listener and key listener classes
-	//declarations
-	//i hope this works
+	// declarations
 	JFrame frame, aboutFrame;
 	JMenuItem newItem, openItem, saveItem, saveAsItem, undoItem, redoItem, aboutItem, syncItem, exitItem;
 	JTextPane textPane;
@@ -54,7 +53,7 @@ public class GUI implements ActionListener, KeyListener
 
 		if (aboutFrame == null)
 		{
-			
+
 			aboutFrame = new JFrame("About Filer");
 			aboutFrame.setLocationRelativeTo(frame);
 			aboutFrame.setSize(300, 200);
@@ -65,16 +64,16 @@ public class GUI implements ActionListener, KeyListener
 	}
 
 	public GUI()
-	//constructor
+	// constructor
 	{
 		frame = new JFrame();
-		//creates a new j frame
+		// creates a new JFrame
 		timer = new Timer();
-		//timer for use in the idle state display
+		// timer for use in the idle state display
 
 		try
 		{
-			//makes the window look like the native systems windows
+			// makes the window look like the native systems windows
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		}
 		catch (Exception e)
@@ -84,12 +83,12 @@ public class GUI implements ActionListener, KeyListener
 		}
 
 		// statusBar
-		//adding GUI elements
+		// adding GUI elements
 		statusBar = new JPanel();
 		statusBar.setLayout(new BoxLayout(statusBar, BoxLayout.LINE_AXIS));
-		//line axis is a horizontal layout
+		// line axis is a horizontal layout
 		statusBar.setPreferredSize(new Dimension(frame.getWidth(), 20));
-		//as wide as the frame, and 20 pixels tall
+		// as wide as the frame, and 20 pixels tall
 		statusLabel = new JLabel("Status: Idle");
 		wordCountLabel = new JLabel("Words: 0");
 		statusBar.add(Box.createHorizontalStrut(10));
@@ -98,34 +97,34 @@ public class GUI implements ActionListener, KeyListener
 		statusBar.add(new JLabel("|"));
 		statusBar.add(Box.createHorizontalStrut(10));
 		statusBar.add(wordCountLabel);
-		//populate the status bar
+		// populate the status bar
 
 		frame.add(statusBar, BorderLayout.SOUTH);
-		//adds the status bar to the frame on the south side (bottom)
+		// adds the status bar to the frame on the south side (bottom)
 
 		// textPane
-		//create text pane
+		// create text pane
 		textPane = new JTextPane();
-		//add key listener that will allow user to type onto the textpane 
+		// add key listener that will allow user to type onto the textpane
 		textPane.addKeyListener(this);
-		//set font
+		// set font
 		Font font = new Font("Arial", 10, 16);
 		textPane.setFont(font);
-		//create scroll pane and put the text pane into it
+		// create scroll pane and put the text pane into it
 		JScrollPane scrollPane = new JScrollPane(textPane);
 		frame.add(scrollPane);
 
-		//create menu bar
+		// create menu bar
 		JMenuBar menuBar = new JMenuBar();
 
 		// fileMenu
 		JMenu fileMenu = new JMenu("File");
-		
-		//populate menu bar item "File"
+
+		// populate menu bar item "File"
 		newItem = new JMenuItem("New");
 		newItem.addActionListener(this);
 		newItem.setAccelerator(KeyStroke.getKeyStroke('N', KeyEvent.CTRL_DOWN_MASK));
-		
+
 		openItem = new JMenuItem("Open");
 		openItem.addActionListener(this);
 		openItem.setAccelerator(KeyStroke.getKeyStroke('O', KeyEvent.CTRL_DOWN_MASK));
@@ -141,8 +140,8 @@ public class GUI implements ActionListener, KeyListener
 		exitItem = new JMenuItem("Exit");
 		exitItem.addActionListener(this);
 		exitItem.setAccelerator(KeyStroke.getKeyStroke('E', KeyEvent.CTRL_DOWN_MASK));
-		
-		//add populations to the menu
+
+		// add populations to the menu
 		fileMenu.add(newItem);
 		fileMenu.add(openItem);
 		fileMenu.addSeparator();
@@ -153,8 +152,8 @@ public class GUI implements ActionListener, KeyListener
 
 		// editMenu
 		JMenu editMenu = new JMenu("Edit");
-		
-		//populate menu button "edit"
+
+		// populate menu button "edit"
 		undoItem = new JMenuItem("Undo");
 		undoItem.setAccelerator(KeyStroke.getKeyStroke('Z', KeyEvent.CTRL_DOWN_MASK));
 
@@ -165,13 +164,13 @@ public class GUI implements ActionListener, KeyListener
 		editMenu.add(redoItem);
 
 		// networkMenu
-		//populate network button
+		// populate network button
 		JMenu networkMenu = new JMenu("Network");
 		syncItem = new JMenuItem("Sync");
 		networkMenu.add(syncItem);
 
 		// helpMenu
-		//populate help menu
+		// populate help menu
 		JMenu helpMenu = new JMenu("Help");
 		aboutItem = new JMenuItem("About");
 		aboutItem.addActionListener(this);
@@ -185,43 +184,42 @@ public class GUI implements ActionListener, KeyListener
 
 		frame.addWindowListener(new WindowAdapter()
 		{
-			//sets up the system reponce to the exit button being pressed
+			// sets up the system respond to the exit button being pressed
 			public void windowClosing(WindowEvent we)
 			{
-				//this will check if the current entries in the text area has been saved, if not it will bring up the save window
-				//(save, no save, cancel)
-				//will return true if saved, a save is made or the user selects not to save
-				//return false if user presses cancel
+				// this will check if the current entries in the text area has been saved, if not it will bring up the save window
+				// (save, no save, cancel)
+				// will return true if saved, a save is made or the user selects not to save
+				// return false if user presses cancel
 				if (saveCheck()) System.exit(0);
 			}
 		});
-		
-		
-		//WHAT? daniel please comment, is this just prep for the file chooser to be called? 
+
+		// initialize JFileChooser and add file filters for .txt and .java files to it
 		fileChooser = new JFileChooser();
 		FileNameExtensionFilter txtFilter = new FileNameExtensionFilter("TXT files", "txt");
 		FileNameExtensionFilter javaFilter = new FileNameExtensionFilter("JAVA files", "java");
 		fileChooser.addChoosableFileFilter(txtFilter);
 		fileChooser.addChoosableFileFilter(javaFilter);
 
-		//sets the frame size and starting title, center location,
-		//and declares that the "x" button will do nothing initially
+		// sets the frame size and starting title, center location,
+		// and declares that the "x" button will do nothing initially
 		frame.setTitle("Filer - Untitled.txt");
 		frame.setSize(800, 600);
 		frame.setLocationRelativeTo(null);
 		frame.setJMenuBar(menuBar);
 		frame.setDefaultCloseOperation(frame.DO_NOTHING_ON_CLOSE);
 		frame.setVisible(true);
-		
+
 	}
-	
-	//this method is used to update the status label on the lower status bar
+
+	// this method is used to update the status label on the lower status bar
 	private void updateStatus(String s)
 	{
 		statusLabel.setText("Status: " + s);
 	}
-	
-	//when the timer expires the idle command is set to idle
+
+	// when the timer expires the status is set to idle
 	public class Task extends TimerTask
 	{
 		public void run()
@@ -230,7 +228,7 @@ public class GUI implements ActionListener, KeyListener
 		}
 	}
 
-	//this is the method called to start the idle timer for 10 seconds, (10000 miliseconds)
+	// this is the method called to start the idle timer for 10 seconds, (10000 miliseconds)
 	private void startTimer()
 	{
 		timer.scheduleAtFixedRate(new Task(), 0, 10_000);
@@ -241,40 +239,40 @@ public class GUI implements ActionListener, KeyListener
 	public boolean saveCheck()
 	{
 		boolean isDone = true;
-		//issaved boolean is updated through out the program 
+		// isSaved boolean is updated through out the program
 		if (!isSaved)
 		{
-			//dialouge box appears to prompt user
+			// dialog box appears to prompt user
 			int choiceVal = JOptionPane.showConfirmDialog(frame, "Would you like to save?");
 			if (choiceVal == JOptionPane.YES_OPTION)
 			{
-				//user chooses to save
+				// user chooses to save
 				if (!saveFile()) isDone = false;
 			}
-			//user chooses cancel
+			// user chooses cancel
 			else if (choiceVal == JOptionPane.CANCEL_OPTION) isDone = false;
 		}
-		//if the user chooses no the method returns that the data is saved (true)
+		// if the user chooses no the method returns true
 		return isDone;
 	}
-	
-	//reactions to button presses by the user
+
+	// reactions to button presses by the user
 	public void actionPerformed(ActionEvent e)
 	{
-		//open file button
+		// open file button
 		if (e.getSource() == openItem)
 		{
-			//change status
+			// change status
 			updateStatus("Opening file");
-			//opens the file chooser
+			// opens the file chooser
 			int returnVal = fileChooser.showOpenDialog(fileChooser);
 
 			if (returnVal == fileChooser.APPROVE_OPTION)
 			{
-				//returns the file to be opened
+				// set the currently open file to be the selected file
 				openedFile = fileChooser.getSelectedFile();
 
-				//attempt to read file
+				// attempt to read file
 				try
 				{
 					readFile(openedFile);
@@ -285,7 +283,7 @@ public class GUI implements ActionListener, KeyListener
 				}
 			}
 		}
-		//remaining buttons call connected methods
+		// remaining buttons call connected methods
 		else if (e.getSource() == saveAsItem)
 		{
 			saveFileAs();
@@ -309,7 +307,7 @@ public class GUI implements ActionListener, KeyListener
 
 	}
 
-	//original state of program at new file
+	// creates a new file
 	private void newFile()
 	{
 		if (saveCheck())
@@ -320,16 +318,16 @@ public class GUI implements ActionListener, KeyListener
 		}
 	}
 
-	//saving file method
+	// saving file method
 	private boolean saveFile()
 	{
 		updateStatus("Saving File");
 
-		//if the file has not been saved before call the SaveFileAs method
+		// if the file has not been saved before call the SaveFileAs method
 		if (openedFile == null) return saveFileAs();
 		else
 		{
-			//if it is an existing file then the file is simply written to the drive
+			// if it is an existing file then the file is simply written to the drive
 			writeFile(openedFile);
 			return true;
 		}
@@ -341,9 +339,9 @@ public class GUI implements ActionListener, KeyListener
 		updateStatus("Saving File");
 
 		int returnVal = fileChooser.showSaveDialog(fileChooser);
-		//opens window
+		// opens window
 		if (returnVal == fileChooser.APPROVE_OPTION)
-			//user chooses to save item
+		// user chooses to save item
 		{
 			writeFile(fileChooser.getSelectedFile());
 			return true;
@@ -353,15 +351,15 @@ public class GUI implements ActionListener, KeyListener
 
 	}
 
-	//read text from file at print to the text area
+	// read text from file at print in the textPane
 	private void readFile(File file) throws Exception
 	{
-		//creates string
+		// creates String to store textPane data
 		String contents = "";
-		//creates buffered reader
+		// creates buffered reader
 		BufferedReader inputStream = null;
 
-		//attempts to read line into the string
+		// attempts to read line into the string
 		try
 		{
 			inputStream = new BufferedReader(new FileReader(file));
@@ -375,19 +373,19 @@ public class GUI implements ActionListener, KeyListener
 			updateStatus(file.getName() + " opened sucessfully");
 			frame.setTitle("Filer - " + file.getName());
 		}
-		//prints error
+		// prints error
 		catch (Exception e)
 		{
 			e.printStackTrace();
 		}
-		//close stream
+		// close stream
 		finally
 		{
 			if (inputStream != null) inputStream.close();
 		}
 	}
 
-	//write to existing file
+	// write to existing file
 	private void writeFile(File file)
 	{
 		try
@@ -398,16 +396,16 @@ public class GUI implements ActionListener, KeyListener
 			Matcher m = p.matcher(file.getPath());
 			m.find();
 
-			//get the .***
+			// get the .***
 			String descrip = fileChooser.getFileFilter().getDescription().substring(0, 3);
 			String extension = "";
 			if (descrip.equals("TXT")) extension = ".txt";
 			else if (descrip.equals("JAV")) extension = ".java";
 
-			//file name
+			// new file name with selected extension applied
 			file = new File(m.group(0) + extension);
 
-			//outputs test pane contents to the output stream
+			// outputs textPane contents to the file
 			PrintWriter outputStream = new PrintWriter(new FileWriter(file));
 			outputStream.print(textPane.getText());
 			outputStream.close();
@@ -421,36 +419,38 @@ public class GUI implements ActionListener, KeyListener
 		}
 	}
 
-	//word count updater
+	// word count updater
 	private void updateWordLabel()
 	{
 		wordCountLabel.setText("Words: " + findWordCount());
 	}
 
-	//word count
+	// word count
 	private int findWordCount()
 	{
 		return textPane.getText().split("\\s+").length;
 	}
 
-	//detects if the user is typing
+	// detects if the user is typing
 	public void keyPressed(KeyEvent e)
 	{
 		updateStatus("Typing");
+
+		// update the wordCountLabel when space is pressed (new word)
 		if (e.getKeyCode() == KeyEvent.VK_SPACE)
 		{
 			updateWordLabel();
 		}
 	}
 
-	//when key released, start idle timer
+	// when key released, start idle timer
 	public void keyReleased(KeyEvent e)
 	{
 		isSaved = false;
 		startTimer();
 	}
 
-	//method implement
+	// method required by KeyListener interface
 	public void keyTyped(KeyEvent e)
 	{
 
